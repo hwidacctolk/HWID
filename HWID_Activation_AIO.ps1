24a0baccf738a6c4945654213a27bb9126661854
hwidacctolk <114142533+hwidacctolk@users.noreply.github.com>
2022-09-22 19:47:55 +0600

    Create HWID_Activation_AIO.ps1

---
+++ HWID_Activation_AIO.ps1
@@ -0,0 +1,19 @@
+# Enable TLSv1.2 for compatibility with older clients
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://raw.githubusercontent.com/hwidacctolk/HWID/main/HWID_Activation_AIO.cmd'
+
+$FilePath = "$env:TEMP\HWID_Activation_AIO.cmd"
+
+try {
+    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
+} catch {
+    Write-Error $_
+	Return
+}
+
+if (Test-Path $FilePath) {
+    Start-Process $FilePath -Wait
+    $item = Get-Item -LiteralPath $FilePath
+    $item.Delete()
+}
